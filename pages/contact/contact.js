46f4c1b43c107605485b6edd9283b6427f3e0ea9
s <glcsofiane40@gmail.com>
2024-12-06 16:26:27 +0100

    form

--- pages/contact/contact.js
+++ pages/contact/contact.js
@@ -3,9 +3,10 @@ const submitBtn = document.querySelector(".submit-btn");
 const nameInput = document.querySelector("#user_name");
 const emailInput = document.querySelector("#user_email");
 const messageInput = document.querySelector("#message");
+const subjectInput = document.querySelector("#user_subject");
 const publicKey = "O3zBA0bhcz9UNz8le";
 const serviceID = "service_754j0xq";
-const templateID = "template_kuz2sm1";
+const templateID = "template_2s4e93w";
 
 emailjs.init(publicKey);
 
@@ -16,7 +17,8 @@ contactForm.addEventListener("submit", (e) => {
     const inputFields = {
         name: nameInput.value,
         email: emailInput.value,
-        message: messageInput.value
+        message: messageInput.value,
+        subject: subjectInput.value
     }
 
     emailjs.send(serviceID, templateID, inputFields)
@@ -25,9 +27,11 @@ contactForm.addEventListener("submit", (e) => {
             nameInput.value = "";
             emailInput.value = "";
             messageInput.value = "";
+            subjectInput.value = "";
         })
         .catch((error) => {
             console.error(error);
             submitBtn.innerText = "Erreur d'envoi";
         });
 });
+
